c5c9260a4a47eeadf266fa2810b19532e1f1db3f
zx21student016 <zx21student016@colegiovalledelmiro.es>
2023-10-03 09:09:04 +0200

    calculadora1

---
+++ calculadoras.py
@@ -0,0 +1,13 @@
+x = int(input("introduce el primer numero:"))
+z = int(input("introduce el segundo numero:"))
+
+sumar = x + z
+restar = x + z
+
+multiplicar = x + z
+
+print ("la suma es =", sumar)
+
+print ("la resta es =", restar)
+
+print ("la multiplicacion es =", multiplicar)
